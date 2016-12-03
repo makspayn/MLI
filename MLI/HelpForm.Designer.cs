20e526708549841c28beda97a08b5a4f1d58672b
makspayn <makspayn94@yandex.ru>
2016-12-03 09:44:20 +0300

    Разработан интерфейс окна Помощь.

---
+++ MLI/HelpForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MLI
+{
+	partial class HelpForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.rtbHelp = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// rtbHelp
+			// 
+			this.rtbHelp.Location = new System.Drawing.Point(0, 0);
+			this.rtbHelp.Name = "rtbHelp";
+			this.rtbHelp.ReadOnly = true;
+			this.rtbHelp.Size = new System.Drawing.Size(752, 393);
+			this.rtbHelp.TabIndex = 0;
+			this.rtbHelp.Text = "";
+			// 
+			// HelpForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(752, 393);
+			this.Controls.Add(this.rtbHelp);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "HelpForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Помощь";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox rtbHelp;
+	}
+}
---
+++ MLI/HelpForm.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+
+namespace MLI
+{
+	public partial class HelpForm : Form
+	{
+		private static HelpForm instance = null;
+
+		public HelpForm()
+		{
+			InitializeComponent();
+		}
+
+		public static HelpForm getInstance()
+		{
+			if (instance == null)
+			{
+				instance = new HelpForm();
+			}
+			return instance;
+		}
+	}
+}
